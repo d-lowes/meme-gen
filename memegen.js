2fcc34fd127c99a265a65c3c53284f76ee3e7a9c
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-03-24 10:58:20 -0700

    created variables for dom

---
+++ memegen.js
@@ -0,0 +1,7 @@
+const uploadImg = document.querySelector('#upload_img');
+const upperText = document.querySelector('#upper_text');
+const lowerText = document.querySelector('#lower_text');
+const canvas = document.querySelector('#img');
+
+
+
